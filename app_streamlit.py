faac8c6ea0d1a95875eaec37cccb8acff70b9807
Paolo César Salazar Patricio <pc.salazarp@alum.up.edu.pe>
2025-11-17 19:58:32 -0500

    Update app_streamlit.py

--- app_streamlit.py
+++ app_streamlit.py
@@ -16,20 +16,20 @@ def load_data():
     csv_files = [
     "df_final_nlp5_part1.csv",
     "df_final_nlp5_part2.csv",
-    #"df_final_nlp5_part3.csv",
-    #"df_final_nlp5_part4.csv",
-    #"df_final_nlp5_part5.csv",
-    #"df_final_nlp5_part6.csv",
-    #"df_final_nlp5_part7.csv",
-    #"df_final_nlp5_part8.csv",
-    #"df_final_nlp5_part9.csv",
-    #"df_final_nlp5_part10.csv",
-    #"df_final_nlp5_part11.csv",
-    #"df_final_nlp5_part12.csv",
-    #"df_final_nlp5_part13.csv",
-    #"df_final_nlp5_part14.csv",
-    #"df_final_nlp5_part15.csv",
-    #"df_final_nlp5_part16.csv",
+    "df_final_nlp5_part3.csv",
+    "df_final_nlp5_part4.csv",
+    "df_final_nlp5_part5.csv",
+    "df_final_nlp5_part6.csv",
+    "df_final_nlp5_part7.csv",
+    "df_final_nlp5_part8.csv",
+    "df_final_nlp5_part9.csv",
+    "df_final_nlp5_part10.csv",
+    "df_final_nlp5_part11.csv",
+    "df_final_nlp5_part12.csv",
+    "df_final_nlp5_part13.csv",
+    "df_final_nlp5_part14.csv",
+    "df_final_nlp5_part15.csv",
+    "df_final_nlp5_part16.csv",
 ]
     df_list = [pd.read_csv(f) for f in csv_files]
     df = pd.concat(df_list, ignore_index=True)
@@ -414,6 +414,7 @@ else:
 
     st.markdown("---")
     st.caption("Análisis de Confesiones con NLP | Desarrollado con Streamlit")
+
 
 
 
